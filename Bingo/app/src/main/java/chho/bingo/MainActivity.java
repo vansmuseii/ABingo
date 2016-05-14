16feb3e58bc748c962490cf766fb0353bacf4dd6
vansmuseii <vansmuseii@gmail.com>
2016-05-14 11:34:30 -0700

    Update MainActivity

--- Bingo/app/src/main/java/chho/bingo/MainActivity.java
+++ Bingo/app/src/main/java/chho/bingo/MainActivity.java
@@ -2,14 +2,36 @@ package chho.bingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList <Integer> bLine = new ArrayList<>();
+    ArrayList <Integer> iLine = new ArrayList<>();
+    ArrayList <Integer> nLine = new ArrayList<>();
+    ArrayList <Integer> gLine = new ArrayList<>();
+    ArrayList <Integer> oLine = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-   // public boolean checkWinner(BingoCard a, )
+    public void generateNumbers(){
+        for (int i = 1; i < 16; i++){
+            bLine.add(i);
+            iLine.add(i+15);
+            nLine.add(i+30);
+            gLine.add(i+45);
+            oLine.add(i+60);
+        }
+        Collections.shuffle(bLine);
+        Collections.shuffle(iLine);
+        Collections.shuffle(nLine);
+        Collections.shuffle(gLine);
+        Collections.shuffle(oLine);
+    }
+
 }
